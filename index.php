ea7f8d03c62277c4e1894ce56791b1b5819176a5
tmanley1985 <tmanley1985@gmail.com>
2014-08-21 19:01:22 -0400

    Updated index.php with html shell

--- index.php
+++ index.php
@@ -5,7 +5,45 @@
 		<head>
 		</head>
 		<body>
-			
+
+			<!-- The Wrapper Div will center the page-->
+
+			<div id="wrapper">
+
+				<header>
+
+					<div id="logo">
+					</div>
+
+					<nav>
+
+						<ul>
+							<li>Home</li>
+							<li>Registered Gyms</li>
+							<li>Registered Athletes</li>
+							<li>National Champions</li>
+							<li>Officals</li>
+						</ul>
+
+					</nav>
+
+				</header>
+
+				<div class="">
+				</div>
+
+				<!-- Holds the rotating media boxes-->
+
+				<div class="media">
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+
+				</div>
+
+			</div>
 		</body>
 	</html>
 
