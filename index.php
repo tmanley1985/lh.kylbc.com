5c5670f64a5daf70115a697b36e7f711d6ba1db8
tmanley1985 <tmanley1985@gmail.com>
2014-08-28 19:45:08 -0400

    added php include to header and footer files

--- index.php
+++ index.php
@@ -1,47 +1,18 @@
-<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="UTF-8">
-			<link rel="stylesheet" type="text/css" href="css/normalize.css">
-			<link rel="stylesheet" type="text/css" href="css/grid.css">
-			<link rel="stylesheet" type="text/css" href="css/main.css">
-			<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-		</head>			
+<?php
+	require('inc/header.php');
+	require('inc/footer.php')
+?>
+			
 
-		<body>
-
-			<!-- The Wrapper Div will center the page-->
-
-			<div id="wrapper">
-
-				<header>
-					<div class="grid-container">
-						<div class="grid-2" id="logo">
-							<h1>Kentucky Local Boxing Committee</h1>
-						</div>
-						<nav class="grid-8">
-							<!--Navigation Links -->
-							<ul>
-								<li><h3>Home</h3></li>
-								<li><h3>Registered Gyms</h3></li>
-								<li><h3>Registered Athletes</h3></li>
-								<li><h3>National Champions</h3></li>
-								<li><h3>Officals</h3></li>
-								<li><h3>Upcoming Events</h3></li>
-								<li><h3>Event Results</h3></li>
-							</ul>
-						</nav>
-					</div>
-				</header>
-
-				<!-- Big Picture -->
-
-				<div class="">
+				<div class= "carousel">
+					<!-- Rotating pictures-->
+					<!--Call Jquery function here-->
+					<img src="graphics/bigpicture.jpg" alt="National champs">
 				</div>
 
 				<!-- Holds the rotating media boxes-->
 
-				<div class="media">
+				<div class="social_media">
 					<ul>
 						<li></li>
 						<li></li>
@@ -50,6 +21,4 @@
 
 				</div>
 
-			</div>
-		</body>
-	</html>
+		
