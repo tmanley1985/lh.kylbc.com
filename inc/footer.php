a68d8249122aee1d42a50d0b95ba97322d03b662
tmanley1985 <tmanley1985@gmail.com>
2014-09-11 18:01:57 -0400

    Added copyright and site info

--- inc/footer.php
+++ inc/footer.php
@@ -3,10 +3,19 @@
 					<ul>
 						<li><a href="#"><img src="graphics/facebook.png" alt="Facebook"></a></li>
 						<li><a href="#"><img src="graphics/twitter.png" alt="Twitter"></a></li>
+						<li>Connect with us!</li>
 					</ul>
+					<p>&copy 2014</p>
+					<p>Site Design by Thomas Manley</p>
+					
 
 				</footer>
 
 			</div>
+			<script>
+			  $(function() {
+			    $(".rslides").responsiveSlides();
+			  });
+			</script>
 		</body>
 	</html>
