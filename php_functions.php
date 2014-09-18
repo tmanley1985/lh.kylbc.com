f51cc23591b13c6a679524a0b786602fe58ff123
tmanley1985 <tmanley1985@gmail.com>
2014-09-18 18:09:24 -0400

    Added some db functions

--- php_functions.php
+++ php_functions.php
@@ -5,10 +5,32 @@
 	function get_gyms(){
 
 		try{
+			// Instantiate DB Object
+			$db = new PDO('mysql:host=localhost;dbname=kylbc', USER, PASS);
 			// Variable for the sql
 			$sql = "SELECT * FROM gyms";
 			$stmt = $db->query($sql);
-			$gyms = $stmt->FETCH(PDO::FETCH_ASSOC);
+			$gyms = $stmt->fetchALL(PDO::FETCH_ASSOC);
+
+			return $gyms;
+
+			// Execute query
+			}catch(PDOException $e){
+				echo $e->getMessage();
+			}
+
+	}
+
+	function get_events(){
+		try{
+			// Instantiate DB Object
+			$db = new PDO('mysql:host=localhost;dbname=kylbc', USER, PASS);
+			// Variable for the sql
+			$sql = "SELECT * FROM events";
+			$stmt = $db->query($sql);
+			$events = $stmt->fetchALL(PDO::FETCH_ASSOC);
+
+			return $events;
 
 			// Execute query
 			}catch(PDOException $e){
@@ -17,4 +39,10 @@
 
 	}
 	
+	function get_coaches(){
+
+	}
+	 function get_athletes(){
+	 	
+	 }
 ?>
