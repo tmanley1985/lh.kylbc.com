20f8c3d0703b7c05d4049aadb7a30212f4c9e8d9
tmanley1985 <tmanley1985@gmail.com>
2014-10-12 16:58:22 -0400

    Deleted some of the conditionals and placed them in the header.

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 			<!-- Rotating pictures-->
 			<!--Call Jquery function here-->
 			<section class="grid-6 main">
+				<h3>Kentucky Amateur Boxing</h3>
+				
 				<ul class="rslides">
 				  <li><img src="graphics/lexpal.jpg" alt=""></li>
 				  <li><img src="graphics/mali.jpg" alt=""></li>
@@ -14,12 +16,19 @@
 			</section>
 
 			<section class="grid-4 sidebar">
-				<h2>News</h2>
-				<h3>Title</h3>
+				<h2>Who Are We?</h2>
+					
 				<article>
-						
 
-
+					<p>We are our Kentucky's Local Boxing Committee.  We are a  subset of USA
+					Boxing, the umbrella for all amateur boxing in the United States.  Our
+					main goal is to promote amateur boxing while striving to provide a safe,
+					competitive environment for youth.  This website lists all current registered
+					coaches, athletes, gyms and officials as well as events and results.
+					Kentucky is home to numerous amateur national champions as well as four
+					heavyweight proffesional boxing champions; Marvin Hart, Jimmy Ellis, Greg Page
+					and Muhammad Ali.  With your support we can usher in the new generation of
+					champions!</p>
 				</article>
 			</section>
 		</div>
