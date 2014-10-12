6d26dafc12a3d01ac24c8798db8b3f65693ebf16
tmanley1985 <tmanley1985@gmail.com>
2014-10-12 17:15:03 -0400

    Added a check to see if post array was empty

--- login.php
+++ login.php
@@ -2,10 +2,14 @@
 	require('inc/header.php'); 
 ?>
 <?php 
+
 	//Instantiates Admin Object
 	$admin = new Admin($db);
-	//Loads the Post data into the authenticate method from the Admin class
-	$user_pass = $admin->authenticate($_POST['username'], $_POST['password']);
+	if(!empty($_POST)){
+			//Loads the Post data into the authenticate method from the Admin class
+			$user_pass = $admin->authenticate($_POST['username'], $_POST['password']);
+
+	}
 
 ?>
 <form method="post">
