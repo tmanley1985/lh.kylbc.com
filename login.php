bca1d28e747f7942ba260ddbf403045ca4518945
tmanley1985 <tmanley1985@gmail.com>
2014-10-12 16:59:02 -0400

    Added the instantiation of the admin class

--- login.php
+++ login.php
@@ -2,7 +2,9 @@
 	require('inc/header.php'); 
 ?>
 <?php 
+	//Instantiates Admin Object
 	$admin = new Admin($db);
+	//Loads the Post data into the authenticate method from the Admin class
 	$user_pass = $admin->authenticate($_POST['username'], $_POST['password']);
 
 ?>
